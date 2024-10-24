1ac6a929b9c32ed976891342d9c62f65057b04e6
DuckKee <111925005+DuckKee@users.noreply.github.com>
2024-10-24 17:54:53 -0600

    Replaced gpio_interrupt.c

--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -1,46 +1,17 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <pico/sync.h>
-#include <hardware/irq.h>
-#include <pico/util/queue.h>
-#include <pico/multicore.h>
 
-#define IN_PIN 27
-#define OUT_PIN 12
-
-typedef struct
-{
-    uint32_t event_mask;
-} gpio_event_t;
-
-queue_t gpio_event_queue;
-
-void gpio_worker_thread(void)
-{
-    gpio_event_t event;
-    while (1)
-    {
-        if (queue_try_remove(&gpio_event_queue, &event))
-        {
-            if (event.event_mask & GPIO_IRQ_EDGE_RISE)
-            {
-                gpio_put(OUT_PIN, true);
-            }
-            else if (event.event_mask & GPIO_IRQ_EDGE_FALL)
-            {
-                gpio_put(OUT_PIN, false);
-            }
-        }
-        sleep_ms(10);
-    }
-}
-
+int toggle = 1;
 void irq_callback(uint gpio, uint32_t event_mask)
 {
-    if (gpio != IN_PIN)
-        return;
-    gpio_event_t event = {.event_mask = event_mask};
-    queue_try_add(&gpio_event_queue, &event);
+    if (gpio != IN_PIN) return;
+    toggle = !toggle;
+    if (event_mask & GPIO_IRQ_EDGE_RISE) {
+        gpio_put(OUT_PIN, true);
+    } else if (event_mask & GPIO_IRQ_EDGE_FALL) {
+        gpio_put(OUT_PIN, false);
+    }
 }
 
 int main(void)
@@ -52,14 +23,10 @@ int main(void)
 
     gpio_init(OUT_PIN);
     gpio_set_dir(OUT_PIN, GPIO_OUT);
-    gpio_put(OUT_PIN, true);
+    gpio_put(OUT_PIN, toggle);
 
-    queue_init(&gpio_event_queue, sizeof(gpio_event_t), 10);
-
-    multicore_launch_core1(gpio_worker_thread);
-
-    gpio_set_irq_enabled_with_callback(IN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, irq_callback);
-
+    gpio_set_irq_enabled_with_callback(IN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL , true, irq_callback);
     while(1) __wfi();
     return 0;
 }
+
